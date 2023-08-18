b0a40c113bccc43a997fc7da3ddeb95828f360c4
hritik1003 <hritikagarwal96@gmail.com>
2023-08-18 10:12:43 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors:{
+        deepblue: "#02042A",
+        ligreen:"#61CEA6",
+        myblack:"#02042A",
+        knowblue:"#0B72E7",
+        tabbalck:'#162F56',
+        tabtext:'#818597',
+        'bluetwo':'#181c2e',
+
+      },
+      spacing:{
+        '128':'48rem',
+        '1080':'1080px',
+        '466':'466px',
+        '350':'350px',
+        '225':'225px',
+        '170':'170px',
+        '77' :'170px',
+        
+        
+
+
+      },
+      rotate:{
+        '60':'37deg',
+      }
+    },
+  },
+  plugins: [],
+}
+
